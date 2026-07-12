35a1c3e82018a5667dbcf749094ca544da331a5c
ankurkundu216-del <ankurkundu216@gmail.com>
2026-07-12 19:04:09 +0530

    Time: 54 ms (15.27%), Space: 10.3 MB (99.4%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+     if(n==1) return 1;
+     int res = 0;
+     
+     for(int i=0; i<n; i++) {
+         for(int j=i+1; j<n; j++) {
+             int cnt = 2;
+             
+             int dx = points[j][0]-points[i][0];
+             int dy = points[j][1]-points[i][1];
+             
+             for(int k=0; k<n; k++) {
+                 if(k!=j && k!=i) {
+                     int dx_ = points[k][0]-points[i][0];
+                 int dy_ = points[k][1]-points[i][1];
+                 if(dy*dx_ == dx*dy_) {
+                     cnt++;
+                 }
+                 }
+             }
+             res = max(res,cnt);
+         }
+     }
+     return res;
+    }
+};
