9f7d01210ce4ed1ed6d5050bf33107dc24d58c0f
AnkurKundu <ankurkundu216@gmail.com>
2026-08-09 23:32:19 +0530

    Time: 6 ms (80.94%), Space: 13.9 MB (35.31%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,39 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-    int memo[2][101][101];
-
-    int n;
-
-    int solveForAlice(vector<int>& piles, int person, int i, int M){
-
-        if(i==n) return 0;
-
-        if(memo[person][i][M] != -1) return memo[person][i][M];
-
-        int result = (person == 1) ? -1 : INT_MAX;
-
-        int stones =0;
-
-        for(int x=1; x <= min(2*M,n-i); x++){
-
-            stones += piles[i+x-1];
-
-            if(person == 1) {
-                result = max(result, stones + solveForAlice(piles, 0, i+x, max(M, x)));
-            } else {
-                result = min(result,solveForAlice(piles,1,i+x,max(M,x)));
-            }
-        }
-        return memo[person][i][M] = result;
-    }
 public:
     int stoneGameII(vector<int>& piles) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        memset(memo, -1, sizeof(memo));
-        n = piles.size();
+        int n = piles.size();
 
-        return solveForAlice(piles,1,0,1);
+        vector<int> suffixSum(n+1,0);
+        for(int i=n-1; i>=0; i--) {
+            suffixSum[i] = suffixSum[i+1]+piles[i];
+        }
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+
+        for(int i=n-1; i>=0; i--){
+            for(int M=1; M<=n; M++){
+                if(i+2*M >= n){
+                    dp[i][M] = suffixSum[i];
+                    continue;
+                }
+                int maxStones=0;
+                for(int x=1; x<=2*M; x++){
+                    maxStones = max(maxStones,suffixSum[i]-dp[i+x][max(M,x)]);
+                }
+                dp[i][M] = maxStones;
+            }
+        }
+        return dp[0][1];
     }
 };
