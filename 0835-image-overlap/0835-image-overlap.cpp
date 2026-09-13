0839ade63cd456082f479d0bc0b8c75b6a45ee49
AnkurKundu <ankurkundu216@gmail.com>
2026-09-13 14:33:36 +0530

    Time: 0 ms (100%), Space: 13.3 MB (54.04%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,46 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-private:
-    int countOverlaps(vector<vector<int>>& img1, vector<vector<int>>& img2, int rowOff, int colOff){
-        int n=img1.size();
-        int cnt=0;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                // find indices of img2 from img1 ka indices[i][j]
-                /*
-                A[i][j] -> B[i+rowOff][j+colOff]
-                */
-
-                int img2_i = i+rowOff;
-                int img2_j = j+colOff;
-
-                if(img2_i<0 || img2_i>=n || img2_j<0 || img2_j>=n){
-                    continue;
-                }
-                if(img1[i][j]==1 && img2[img2_i][img2_j]==1){
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         int n=img1.size();
-        // nxn matrix
-        // row = n
-        // col = n
+
+        vector<int> A(n,0);
+        vector<int> B(n,0);
+
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<n; ++j){
+                if(img1[i][j]) A[i] |= (1<<j);
+                if(img2[i][j]) B[i] |= (1<<j);
+            }
+        }
         int maxOverlap=0;
 
-        for(int rowOff=-n+1; rowOff<n; rowOff++){
-            for(int colOff=-n+1; colOff<n; colOff++){
-                int cnt=countOverlaps(img1, img2, rowOff, colOff);
-                
-                maxOverlap=max(maxOverlap, cnt);
+        for(int rowShift=-n+1; rowShift<n; ++rowShift){
+            for(int colShift=-n+1; colShift<n; ++colShift){
+                int currentOverlap=0;
+                for(int i=0; i<n; ++i){
+                    int img2Row=i+rowShift;
+                    if(img2Row<0 || img2Row>=n) continue;
+                    int shiftedRowA;
+                    if(colShift >= 0){
+                        shiftedRowA=A[i]<<colShift;
+                    }else{
+                        shiftedRowA=A[i]>>(-colShift);
+                    }
+                    currentOverlap += __builtin_popcount(shiftedRowA & B[img2Row]);
+                }
+                maxOverlap = max(maxOverlap, currentOverlap);
             }
         }
         return maxOverlap;
